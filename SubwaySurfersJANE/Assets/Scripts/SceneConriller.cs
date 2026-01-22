c9e43bd58c0aa14a1506547831160fabbd920e0f
ricveldre <fernando.martinez@uteca.edu.mx>
2026-01-21 20:32:27 -0600

    hicimos GAME terminamos INTRO

---
+++ SubwaySurfersJANE/Assets/Scripts/SceneConriller.cs
@@ -0,0 +1,25 @@
+using UnityEngine; 
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Sincontroll : MonoBehaviour
+{
+    [SerializeField]
+
+private Animator fade;
+    [SerializeField]
+
+    private string fadeAnimationName;
+
+    public void GoToSceneWithFade(string sceneName)
+    {
+        StartCoroutine(LoadSceneAfterFade(sceneName));
+    }
+
+  private IEnumerator LoadSceneAfterFade(string sceneName)
+  {
+    fade.Play(fadeAnimationName, 0, 0f);
+    yield return new WaitForSeconds(fade.GetCurrentAnimatorStateInfo(0).length);
+    SceneManager.LoadScene(sceneName);
+  }
+}
